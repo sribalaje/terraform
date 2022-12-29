f30044ef1d584aeee19600f4b6a2f9f28afa5139
sribalaje <root@ip-172-31-91-155.ec2.internal>
2022-12-29 05:25:53 +0000

    0

---
+++ map.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = var.regions
+}
+
+variable "regions" {
+  default = "ap-south-1"
+}
+
+variable "mapping" {
+  type = map
+  default = {
+    us-east-1 = "ami-0b5eea76982371e91",
+    us-east-2 = "ami-0a606d8395a538502",
+    ap-south-1 = "ami-0cca134ec43cf708f"
+}
+}
+
+variable "zone" {
+  default = {
+  us-east-1 = "us-east-1c"
+  us-east-2 = "us-east-2c"
+  ap-south-1 = "ap-south-1b"  
+}
+}
+
+
+resource "aws_instance" "terraform_ec2" {
+  ami = "${lookup(var.mapping,var.regions)}"
+  instance_type  = "t2.micro"
+  availability_zone = "${lookup(var.zone,var.regions)}"
+  tags = {
+    Name = "mumbai"
+  }
+}
+
